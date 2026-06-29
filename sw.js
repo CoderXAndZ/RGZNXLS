8aa14b7d8f11822fba372106077b503187a3541f
CoderXAndZ <1935722630@qq.com>
2026-06-29 14:48:20 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'ai-trainer-quiz-v1';
+const ASSETS = ['./index.html', './sw.js', './manifest.webmanifest'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((hit) => hit || fetch(e.request))
+  );
+});
